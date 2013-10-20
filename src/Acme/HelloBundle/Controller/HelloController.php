89d5cfe7b0d89448f85a3e28153fa9fb54267fc2
julianjjo <julian.mican@gmail.com>
2013-10-19 21:58:47 -0500

    segundo_commit

---
+++ src/Acme/HelloBundle/Controller/HelloController.php
@@ -0,0 +1,14 @@
+<?php
+// src/Acme/HelloBundle/Controller/HelloController.php
+namespace Acme\HelloBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+ 
+class HelloController extends Controller
+{
+    public function indexAction($name)
+    {
+         return $this->render('AcmeHelloBundle:Hello:index.html.twig', array('name' => $name));
+    }
+}
+?>
